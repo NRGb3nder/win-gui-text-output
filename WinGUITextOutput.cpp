acddd9dc2d7037b7fe8c213d03a15fbddb0a47af
NRGb3nder <energybender120@gmail.com>
2018-09-10 06:03:57 +0300

    Use TextTable class in application

--- WinGUITextOutput.cpp
+++ WinGUITextOutput.cpp
@@ -1,8 +1,9 @@
 ﻿#include <windows.h>
-#include "TableDrawingRoutines.h"
+#include <tchar.h>
+#include "TextTable.h"
 
-#define WINDOW_CLASS_NAME L"WinGUITextOutputMainWindow"
-#define WINDOW_TITLE L"Windows GUI Text Output Lab"
+#define WINDOW_CLASS_NAME _T("WinGUITextOutputMainWindow")
+#define WINDOW_TITLE _T("Windows GUI Text Output Lab")
 #define DEFAULT_WINDOW_WIDTH 1000
 #define DEFAULT_WINDOW_HEIGHT 500
 #define MIN_WINDOW_WIDTH 400
@@ -12,27 +13,28 @@
 #define IDM_AUTHOR 0x01
 #define IDM_OPEN 0x02
 
-#define FILE_MENU_ITEM_TITLE L"File"
-#define FILE_OPEN_MENU_ITEM_TITLE L"Open"
-#define HELP_MENU_ITEM_TITLE L"Help"
-#define AUTHOR_MENU_ITEM_TITLE L"Author"
+#define FILE_MENU_ITEM_TITLE _T("File")
+#define FILE_OPEN_MENU_ITEM_TITLE _T("Open")
+#define HELP_MENU_ITEM_TITLE _T("Help")
+#define AUTHOR_MENU_ITEM_TITLE _T("Author")
 
-#define TEXT_HELP L"No help here right now."
-#define TEXT_ABOUT_AUTHOR L"Arseni Rynkevich\r\nStudent group 651003\r\nhttps://github.com/NRGb3nder"
+#define TEXT_HELP _T("No help here right now.")
+#define TEXT_ABOUT_AUTHOR _T("Arseni Rynkevich\r\nStudent group 651003\r\nhttps://github.com/NRGb3nder")
 
 #define TABLE_HORIZONTAL_MARGIN (MIN_WINDOW_WIDTH / 10)
 #define TABLE_VERTICAL_MARGIN (MIN_WINDOW_HEIGHT / 10)
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
-VOID AddMenu(HWND hWnd);
-VOID Paint(HWND hWnd);
+void AddMenu(HWND hWnd);
+void OnPaint(HWND hWnd);
 
 HMENU hMenu;
 HMENU hFileSubMenu;
+HFONT hFont;
 
-BOOL useDoubleBuffering = TRUE;
+bool useDoubleBuffering = true;
 
-INT CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, INT nCmdShow)
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     WNDCLASS wndc = { 0 };
     wndc.style = CS_HREDRAW | CS_VREDRAW;
@@ -45,7 +47,7 @@ INT CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
     RegisterClass(&wndc);
 
-    HWND hWnd = CreateWindow(WINDOW_CLASS_NAME, WINDOW_TITLE, WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+    const HWND hWnd = CreateWindow(WINDOW_CLASS_NAME, WINDOW_TITLE, WS_OVERLAPPEDWINDOW | WS_VISIBLE,
         CW_USEDEFAULT, CW_USEDEFAULT, DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, NULL, NULL, hInstance, NULL);
 
     ShowWindow(hWnd, nCmdShow);
@@ -67,8 +69,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     switch (message) {
     case WM_CREATE:
         AddMenu(hWnd);
+        hFont = CreateFont(0, 0, 0, 0, 0, 0, 0, 0, DEFAULT_CHARSET, 0, 0, 0, 0, "Arial");
         break;
     case WM_DESTROY:
+        DeleteObject(hFont);
         PostQuitMessage(0);
         break;
     case WM_GETMINMAXINFO:
@@ -87,7 +91,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         }
         break;
     case WM_PAINT:
-        Paint(hWnd);
+        OnPaint(hWnd);
         break;
     default:
         return DefWindowProc(hWnd, message, wParam, lParam);
@@ -96,7 +100,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-VOID AddMenu(HWND hWnd)
+void AddMenu(HWND hWnd)
 {
     hMenu = CreateMenu();
     hFileSubMenu = CreatePopupMenu();
@@ -107,17 +111,18 @@ VOID AddMenu(HWND hWnd)
     SetMenu(hWnd, hMenu);
 }
 
-VOID Paint(HWND hWnd)
+void OnPaint(HWND hWnd)
 {
     RECT wndRect;
     GetClientRect(hWnd, &wndRect);
-    INT widthWnd = wndRect.right - wndRect.left;
-    INT heightWnd = wndRect.bottom - wndRect.top;
+    const int wdthWnd = wndRect.right - wndRect.left;
 
-    INT widthTable = widthWnd - 2 * TABLE_HORIZONTAL_MARGIN;
-    INT heightTable = heightWnd - 2 * TABLE_VERTICAL_MARGIN;
-    DrawTable(hWnd,
-        TABLE_HORIZONTAL_MARGIN, TABLE_VERTICAL_MARGIN,
-        widthTable, heightTable,
-        useDoubleBuffering);
+   const int wdthTable = wdthWnd - 2 * TABLE_HORIZONTAL_MARGIN;
+    TableContent content = { 
+        { _T("Oneasfasfasfafasf"), _T("Twasfasfasfasfasfasfaso"), _T("Thrasfasffsfsee") },
+        { _T("Fosfsfsfsfsfur"), _T("Fsfihashashahve") },
+        { _T("Shahhahashsahashahashashasashaix"), _T("Seven"), _T("Eighsahashashasht"), _T("Nihne") }
+    };
+    TextTable *textTable = new TextTable(content, TABLE_HORIZONTAL_MARGIN, TABLE_VERTICAL_MARGIN, wdthTable, hFont, false);
+    textTable->Draw(hWnd);
 }
